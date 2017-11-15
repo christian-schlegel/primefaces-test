61171fdebfe223c01a2d79e0ef84452ed3bd41a6
Christian Schlegel <33683165+christian-schlegel@users.noreply.github.com>
2017-11-15 11:10:01 +0100

    Add example issue for p:fileDownload with p:commandLink and f:param

--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -4,16 +4,23 @@ import java.io.Serializable;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import java.io.ByteArrayInputStream;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
+
 
 @ManagedBean(name = "testView")
 @ViewScoped
 public class TestView implements Serializable {
     
     private String testString;
+
+    private StreamedContent streamedContent;
     
     @PostConstruct  
     public void init() {
         testString = "Welcome to PrimeFaces!!!";
+        streamedContent = new DefaultStreamedContent(new ByteArrayInputStream("Test".getBytes()), "text/plain", "test.txt");
     }
 
     public String getTestString() {
@@ -23,4 +30,9 @@ public class TestView implements Serializable {
     public void setTestString(String testString) {
         this.testString = testString;
     }    
+
+    public StreamedContent getStreamedContent() {
+        return streamedContent;
+    }
+
 }
